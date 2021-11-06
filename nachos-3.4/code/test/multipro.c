4446f94ea0ae124a9210c8109a36a6b2ac7f1a51
itreallydobefrank <tempanarof@yahoo.com>
2021-11-05 20:12:07 -0500

    Test commit

--- nachos-3.4/code/test/multipro.c
+++
@@ -1,9 +0,0 @@
-#include "syscall.h"
-
-int main() {
-	Write("\nExec Adder 1", 20, ConsoleOutput);
-	Exec("../test/adder");
-	Yield();
-	Write("\nExec Adder 2", 20, ConsoleOutput);
-	Exec("../test/adder");
-}
